a43769a738c20e715c3ef985795f8959fe402d3f
AnDo <ando@MacBook-Pro-Max.local>
2023-12-20 21:20:13 +0700

    c2

--- src/main/java/bean/BeanTest.java
+++ src/main/java/bean/BeanTest.java
@@ -1,16 +1,8 @@
 package bean;
 
-import javax.annotation.ManagedBean;
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
-import javax.inject.Named;
-import javax.servlet.ServletOutputStream;
 import java.io.Serializable;
 
-//@ManagedBean
-//@Named("beanTest")
-//@RequestScoped
-public class BeanTest implements Serializable {
+public class BeanTest {
     String name;
 
     public BeanTest() {
